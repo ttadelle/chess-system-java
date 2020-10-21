c298a15819634285c14a550ff1e4dfadfe6dc52f
Thiago Tadelle <thiago.tadelle@gmail.com>
2020-10-21 15:53:16 -0300

    Manipulação de exceção e limpeza da tela

--- src/application/UI.java
+++ src/application/UI.java
@@ -28,12 +28,17 @@ public class UI {
 	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 	
+	public static void clearScreen() {
+		System.out.println("\033[H\033[2J");
+		System.out.flush();
+	}
+	
 	public static ChessPosition readChessPosition(Scanner sc) {
 		try {
-		String s = sc.nextLine();
-		char column = s.charAt(0);
-		int row = Integer.parseInt(s.substring(1));
-		return new ChessPosition(column, row);
+			String s = sc.nextLine();
+			char column = s.charAt(0);
+			int row = Integer.parseInt(s.substring(1));
+			return new ChessPosition(column, row);
 		}
 		catch (RuntimeException e) {
 			throw new InputMismatchException("Erro ao ler a posição do xadrez: Valores válidos de a1 até h8.");
